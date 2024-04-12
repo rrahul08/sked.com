e0f26327fd19cfab587dba1c5b47856b3afd240a
rrahul08 <thefirstclass555@gmail.com>
2024-04-12 17:19:25 +0530

    created welcome page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from './Components/Login';
+import Welcome from './Components/Welcome';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    //   <Welcome/>
+    // </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Welcome/>}/>
+        <Route path='/login' element={<Login/>}/>
+      </Routes>
+    </Router>
   );
 }
 
